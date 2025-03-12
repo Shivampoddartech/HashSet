05b9f06c2901dafcb76c92498499aaee5f204125
shivam <shivampoddartech@gmail.com>
2025-03-12 18:46:42 +0530

    Example HashSet

---
+++ Main.java
@@ -0,0 +1,20 @@
+package hasSet;
+
+public class Main {
+	public static void main(String[] args) {
+		String a = "A";
+		String b = "B";
+
+		System.out.println(a.compareTo(b));
+		System.out.println(b.compareTo(a));
+		System.out.println(a.compareTo(a));
+
+		System.out.println("------------");
+		Integer c = 10;
+		Integer d = 20;
+		System.out.println(a.compareTo(a));
+		System.out.println(a.compareTo(b));
+		System.out.println(b.compareTo(a));
+
+	}
+}
