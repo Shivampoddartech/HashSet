2499679ad299708f7f5b594d23b86d499dc286e5
shivam <shivampoddartech@gmail.com>
2025-03-12 17:12:19 +0530

    HashSet Example

---
+++ Demo.java
@@ -0,0 +1,15 @@
+package hasSet;
+
+import java.util.HashSet;
+
+public class Demo {
+	public static void main(String[] args) {
+		HashSet h = new HashSet();
+		h.add(10);
+		h.add(20.20);
+		h.add(null);
+		h.add("Java");
+		h.add(10);
+		System.out.println(h);
+	}
+}
